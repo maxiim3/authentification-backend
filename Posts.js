99a6d9db787d86ecdca50bdd953955249f40b96f
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-02-01 18:28:46 +0100

    add express

---
+++ Posts.js
@@ -0,0 +1,49 @@
+
+const htmlTemplate = (title, content) => {
+    return `
+    <style>
+        body{
+            display: grid;
+            place-content: center;
+            background: #edeaea;
+        }
+        .container {
+            display: flex;
+            flex-flow: column;
+            text-align: center;
+            gap: 8px;
+            justify-content: center;
+            width: 350px;
+            height: 320px;
+            border-radius: 12px;
+            background-color: #f8f4f4;
+            box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+        }
+        
+        .title {
+            font-size: 1.5rem;
+            font-weight: 600;
+            color: orange;
+        }
+        
+        .content {
+            font-size: 1rem;
+            font-weight: 400;
+            color: black;
+        }
+    </style>
+    
+      <article class="container">  
+        <h1 class="title">${title}</h1>
+        <p class="content">${content}</p>
+    </article>
+    `;
+}
+
+const posts = [
+    {id: 1, title: htmlTemplate("First Post", "This is the first post")},
+    {id: 2, title: htmlTemplate("Second Post", "This is the second post")},
+    {id: 3, title: htmlTemplate("Third Post", "This is the third post")},
+]
+
+module.exports.posts = {posts, htmlTemplate}
--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-const http = require('http');
+// Create an express app
+const express = require('express');
+// Load Joi
+const Joi = require('joi');
+// Load env variables
+const dotenv = require('dotenv');
+// get paths
+const config = require('./config.json');
+// Posts
+const {posts, htmlTemplate} = require('./Posts');
 
-const server = http.createServer((req, res) => {
-    switch (req.url) {
-        case '/':
-            res.write('Hello World');
-            res.end();
-            break;
-        case '/api/courses':
-            res.write(JSON.stringify([1, 2, 3]));
-            res.end();
-            break;
-        default:
-            res.write('Not Found');
-            res.end();
-    }
+const app = express()
+app.use(express.json())
+dotenv.config();
+const PORT = process.env.PORT || 3001
+
+
+// Create Root Route
+app.get(config.paths.root, (req, res) => {
+    res.send("<h1>Root Page</h1>")
 })
 
-server.listen(3000);
-console.log('Listening on port 3000...');
+
+app.get(`${config.paths.posts}/:id?`, (req, res) => {
+    const paramId = parseInt(req.params.id)
+    const requestedPost = posts.find(({id}) => id === paramId)
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.valid(req.body, schema)
+    console.log(result)
+
+    if (!requestedPost)
+        return res.status(404).send("The post with the given ID was not found.")
+
+    return res.send(requestedPost.title)
+})
+
+app.post(config.paths.posts, (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.valid(req.body, schema)
+    console.log(result)
+
+        const post = {
+            id: posts.length + 1,
+            title: req.body
+        }
+        posts.push(post)
+        res.send(post)
+})
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+})
